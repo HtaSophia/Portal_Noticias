ac280012b794dfec15fb1089c09aa41a57038656
Aghata <31016944+HtaSophia@users.noreply.github.com>
2019-03-29 11:50:12 -0300

    Add files via upload

---
+++ config/dbConnection.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+
+var connMySQL = function(){
+    return mysql.createConnection({
+        host: 'localhost',
+        user: 'hta',
+        password: 'BVYoge)544',
+        database: 'portal_noticias'
+    });
+}
+module.exports = function(){
+    return connMySQL;
+};
